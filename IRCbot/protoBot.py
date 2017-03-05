e58d635322d36458cc4c56e910de85746c322daa
Sascha Klein <Sascha Klein>
2017-03-05 15:37:27 +0100

    Added comments and stuff...

--- IRCbot/protoBot.py
+++ IRCbot/protoBot.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python2
 
+# This File is an example for a very simple IRC BOT.
+# Maybe this should be turned into an object oriented version,
+# And this definitely needs DocStrings and stuff!
+
 import sys
 import socket
 
 def main():
   
+  # Parsing Commandline Parameters
+
   if len(sys.argv) != 4:
     
     print 'Usage: ./protoBot.py HOST PORT CHANNEL'
@@ -18,6 +24,8 @@ def main():
   else:
     CHAN = sys.argv[3]
 
+  # Setting Bot Parameters
+  
   PASS = 'none'
 
   BOTNICK = 'Test'
@@ -25,9 +33,12 @@ def main():
   REALNAME = 'Just a framework'
   TOPIC = 'Bot Testing. YAY!'
 
-  data = ''
+  # Defining the type of connection
 
+  data = ''
   irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  # Defining functions for main bot functionality
 
   def action(channel, action):
     irc.send('PRIVMSG ' +channel+ ' :\x01ACTION '+ action + ' \x01' + '\r\n')	
@@ -49,40 +60,64 @@ def main():
     irc.send('NICK '+nick+'\r\n')
     irc.send('USER '+ident+' '+host+' bla :'+realname+'\r\n')
 
+
+  # Connecting to the Server and channel we got from the commandline parameters
+
   ircConnect(HOST, PORT, BOTNICK, IDENT, REALNAME)
   joinChan(CHAN)
   setTopic(CHAN, TOPIC)
 	
-  while 1:
+  # Starting the actual main Bot-Loop :)
+
+  while True:
     data = irc.recv(1024)
     print data
     tmp = data.split()
 
+    # If we receive a PING, we need to answer with PONG and the value we got from the Server.
+    # Otherwise, the Server would close the connection on us...
+
     if tmp[0] == 'PING':
       print ('PONG ' + tmp[1] + '\r\n')
       irc.send('PONG ' + tmp[1] + '\r\n')
+    
+    # If we're being kicked off the channel, we rejoin and send a message.
 
     if tmp[1] == 'KICK':
       joinChan(CHAN)
       chanMsg(CHAN, 'THAT was uncalled for...')
-      
+    
+    # If we see the BotMaster join the channel, we'll greet him.
+    # The nick of the Botmaster should be turned into a variable, maybe.
+
     if tmp[1] == 'JOIN' and tmp[0].split('!')[0] == (':TRiGGER80'):
       chanMsg(CHAN, 'TRiGGER80 is the OP of this channel.')
       irc.send('MODE ' +CHAN+ ' +o TRiGGER80' + '\r\n')
       action(CHAN, 'Salutes.')
+    
+    # Checking if someone is talking to us in the Channel or directly.
+    # We'll reply in the same manner.
+
+    # Replying, if we're talked to in the channel.
 
     if tmp[1] == 'PRIVMSG' and tmp[2] == CHAN and BOTNICK in data:
       chanMsg(CHAN, 'Receiving message in public.')
+
+    # Replying if we're being addressed in a Private Message.
 
     if tmp[1] == 'PRIVMSG' and tmp[2] == BOTNICK:
       tmp=tmp[0].split('!')
       tmp=tmp[1].split('@')
       chanMsg(tmp[0], 'Receiving message on private.')
+  
+  # If we're gonna exit at all, we'll exit cleanly.
+  # Maybe i should add a function / command for the bot, that will make it quit.
 
   sys.exit()
   
     
     
 # This is the standard boilerplate that calls the main() function.
+
 if __name__ == '__main__':
   main()
